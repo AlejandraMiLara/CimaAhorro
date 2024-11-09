ca8007d2799f0b5fe4fe2956da1c53a1c152bc75
AlejandraMiLara <a365544@uabc.edu.mx>
2024-11-09 11:43:29 -0800

    registro/ingreso

--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,57 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.http import HttpResponse
 
-# Create your views here.
-def Home(request):
-    return render(request, 'home.html')
+def inicio(request):
+    return render(request, 'inicio.html')
 
-def SignUp(request):
-    return render(request, 'signup.html', {
-        'form':UserCreationForm
-    })
+def registro(request):
 
-def SignIn(request):
-    return render(request, 'signin.html')
+    if request.method == 'GET':
+        return render(request, 'registro.html', {
+            'form':UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('panel')
+            except:
+                return render(request, 'registro.html', {
+                    'form':UserCreationForm,
+                    'msj': 'El nombre de usuario ya existe'
+                })
+            
+        else:
+            return render(request, 'registro.html', {
+                'form':UserCreationForm,
+                'msj': 'Los passwords no son iguales'
+            })
+
+def ingreso(request):
+
+    if request.method == 'GET':
+        return render(request, 'ingreso.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'ingreso.html', {
+                'form': AuthenticationForm,
+                'msj': 'Usuario o password incorrecto'
+            })
+        else:
+            login(request, user)
+            return redirect('panel')
+
+def salir(request):
+    logout(request)
+    return redirect('ingreso')
+
+def panel(request):
+    return render(request, 'panel.html')
